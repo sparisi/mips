5d4d56c5d63df3bccf3eb19dc3b7a9814c5f92f6
Simone <parisi@ias.tu-darmstadt.de>
2015-08-07 03:20:10 +0200

    function for normalization of single rewards

--- MO_Library/normalizeFront.m
+++ MO_Library/normalizeFront.m
@@ -1,5 +1,6 @@
 function fn = normalizeFront(f)
 % Normalizes a frontier in order to have all the objectives in [0,1].
+% The normalization is: fn = (f - utopia) / (antiutopia - utopia)
 
 utopia = max(f);
 antiutopia = min(f);
