d40a8941ea495e94841b0a11179de28243a077bb
Simone <parisi@ias.tu-darmstadt.de>
2015-12-05 00:41:18 +0100

    renaming

--- Library/Policies/Discrete/gibbs.m
+++ Library/Policies/Discrete/gibbs.m
@@ -7,7 +7,7 @@ classdef gibbs < policy_discrete
     end
     
     properties(GetAccess = 'public', SetAccess = 'public')
-        inverse_temperature
+        epsilon % temperature (low value -> deterministic policy)
     end
     
     methods
@@ -19,14 +19,14 @@ classdef gibbs < policy_discrete
             obj.basis = basis;
             obj.theta = theta;
             obj.action_list = action_list;
-            obj.inverse_temperature = 1;
+            obj.epsilon = 1;
             obj.dim_explore = 0;
             obj.dim = 1;
         end
   
         function [prob_list, qfun] = distribution(obj, States)
             nstates = size(States,2);
-            it = obj.inverse_temperature;
+            temperature = obj.epsilon;
             nactions = length(obj.action_list);
             num = zeros(nactions, nstates);
             qfun = zeros(nactions, nstates);
@@ -37,7 +37,7 @@ classdef gibbs < policy_discrete
                 loc_phi = zeros(dphi*(nactions-1),nstates);
                 loc_phi((i-1)*dphi+1:(i-1)*dphi+dphi,:) = phi;
                 loc_Q = obj.theta'*loc_phi;
-                num(i,:) = exp(it*loc_Q);
+                num(i,:) = exp(loc_Q/temperature);
                 qfun(i,:) = loc_Q;
                 den = den + num(i,:);
             end
@@ -63,7 +63,7 @@ classdef gibbs < policy_discrete
             idx = find(obj.action_list == action);
             assert(length(idx) == 1);
             
-            it = obj.inverse_temperature;
+            temperature = obj.epsilon;
             nactions = length(obj.action_list) - 1;
             exp_term = zeros(nactions,1);
             dphi = feval(obj.basis);
@@ -73,8 +73,8 @@ classdef gibbs < policy_discrete
                 loc_phi = zeros(dphi*nactions,1);
                 loc_phi((i-1)*dphi+1:(i-1)*dphi+dphi) = phi;
                 loc_Q = obj.theta'*loc_phi;
-                exp_term(i) = exp(it*loc_Q);
-                phi_term(:,i) = it*loc_phi;
+                exp_term(i) = exp(loc_Q/temperature);
+                phi_term(:,i) = loc_phi/temperature;
             end
             
             idx = isinf(exp_term);
@@ -92,7 +92,7 @@ classdef gibbs < policy_discrete
                 loc_phi = zeros(dphi*nactions,1);
                 i = find(obj.action_list == action);
                 loc_phi((i-1)*dphi+1:(i-1)*dphi+dphi) = phi;
-                dlpdt = it*loc_phi - num ./ (1 + den);
+                dlpdt = loc_phi/temperature - num ./ (1 + den);
             end
         end
         
@@ -120,7 +120,7 @@ classdef gibbs < policy_discrete
         end
         
         function obj = makeDeterministic(obj)
-            obj.inverse_temperature = 1e8;
+            obj.epsilon = 1e-8;
         end
         
     end
