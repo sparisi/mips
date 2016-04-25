ae0e6e59a678d6c4a1fbd589a15659ca8f1079d3
Simone <parisi@ias.tu-darmstadt.de>
2016-04-25 18:48:13 +0200

    MORE by Abdomaleki, NIPS 2015

---
+++ Algs/Episodic/MORE_Solver.m
@@ -0,0 +1,142 @@
+classdef MORE_Solver < handle
+% Model-based Relative Entropy Stochastic Search.
+%
+% =========================================================================
+% REFERENCE
+% A Abdolmaleki, R Lioutikov, N Lau, L P Reis, J Peters, G Neumann
+% Model-based Relative Entropy Stochastic Search (2015)
+    
+    properties
+        epsilon % KL divergence bound
+        gamma   % Decreasing factor for the bound on the relative entropy
+        minH    % Minimum entropy
+        b       % Mean of the Gaussian search distribution
+        Q       % Covariance of the Gaussian
+        Qi      % Inverse of the covariance (store it for faster computation)
+        model   % Quadratic reward model
+    end
+    
+    methods
+        
+        %% CLASS CONSTRUCTOR
+        function obj = MORE_Solver(epsilon, gamma, minH, policy)
+            obj.epsilon = epsilon;
+            obj.gamma = gamma;
+            obj.minH = minH;
+            assert(isa(policy,'Gaussian'), 'The search distribution must be a Gaussian.')
+            obj.Q = policy.Sigma;
+            obj.b = policy.mu;
+            obj.Qi = inv(obj.Q);
+            obj.model.R = zeros(policy.daction);
+            obj.model.r = zeros(policy.daction,1);
+            obj.model.r0 = 0;
+        end
+        
+        %% PERFORM AN OPTIMIZATION STEP
+        function [policy, divKL] = step(obj, J, Actions, policy, W)
+            if nargin < 5, W = ones(1, size(J,2)); end % IS weights
+            obj.model = quadraticfit(Actions, J, ...
+                'weights', W, ...
+                'standardize', 1);
+            
+            % Ensure that R is negative definite -> F is positive definite
+            [U, V] = eig(obj.model.R);
+            V(V > 0) = -1e-8;
+            obj.model.R = U * V * U';
+            obj.model.eval = @(X) sum( (X'*obj.model.R)' .* X, 1 ) + (X'*obj.model.r)' + obj.model.r0';
+            
+            [eta, omega] = obj.optimize;
+            divKL = obj.update(eta, omega);
+            policy = policy.update(obj.b, obj.Q);
+        end
+        
+        %% CORE
+        function [eta, omega] = optimize(obj)
+            % Optimization problem settings
+            options = optimset('GradObj', 'off', ...
+                'Display', 'off', ...
+                'MaxFunEvals', 1000 * 5, ...
+                'Algorithm', 'interior-point', ...
+                'TolX', 10^-8, ...
+                'TolFun', 10^-12, ...
+                'MaxIter', 1000);
+            lowerBound = [1e-8; 1e-8]; % [eta, omega] > 0
+            upperBound = [1e8; 1e8];% [eta, omega] < inf
+            params = [0.5; 0.5]; % init [eta, omega]
+            params = fmincon(@(params)obj.dual(params), ...
+                params, [], [], [], [], lowerBound, upperBound, [], options);
+            eta = params(1);
+            omega = params(2);
+        end
+        
+        %% CLOSED FORM UPDATE
+        function divKL = update(obj, eta, omega)
+            F_new = inv(eta * obj.Qi - 2 * obj.model.R);
+            f_new = eta * (obj.Qi * obj.b) + obj.model.r;
+            Q_new = F_new * (eta + omega);
+            Qi_new = inv(Q_new);
+            b_new = F_new * f_new;
+            [~, p] = chol(Q_new);
+            if(p ~= 0)
+                warning('Covariance is not PSD. Policy has not been updated');
+                divKL = 0;
+            else
+                divKL = 0.5 * ( ...
+                    trace(Q_new \ obj.Q) + (b_new - obj.b)' / Q_new * (b_new - obj.b) ...
+                    - length(b_new) + logdet(Q_new,'chol') - logdet(obj.Q,'chol') ...
+                    );
+                obj.Q = Q_new;
+                obj.Qi = Qi_new;
+                obj.b = b_new;
+            end
+        end
+        
+        %% DUAL FUNCTION
+        function [g, gD] = dual(obj, params)
+            b = obj.b;
+            Q = obj.Q;
+            Qi = obj.Qi;
+            R = obj.model.R;
+            r = obj.model.r;
+            dimQ = size(Q,1);
+
+            eta = params(1);
+            omega = params(2);
+            H = 0.5 * ( dimQ*log(2*pi*exp(1)) + 2*sum(log(diag(chol(Q)))) );
+            beta = (obj.gamma * (H - obj.minH) + obj.minH);
+
+            F = eta * Qi - 2 * R;
+            tempQ = ( F / (eta + omega) );
+            if any(isnan(tempQ) | any(eig(tempQ) <= 0))
+                g = inf;
+                gD = inf(2,1);
+                warning('Covariance is not PSD.')
+                return
+            end
+            
+            f = eta * (Q \ b) + r;
+            
+            logDetQ = dimQ * log(2*pi) + 2*sum(log(diag(chol(Q))));
+            logDetF = dimQ * log((eta+omega)*2*pi) - 2*sum(log(diag(chol(F))));
+            
+            g = eta * obj.epsilon - omega * beta - 0.5 * eta * (b' * (Q \ b) ) ...
+                + 0.5 * f' * (F \ f) - 0.5 * eta * logDetQ + 0.5 * (eta + omega) * logDetF;
+            
+            % Gradient of dual function (if required by the optimizer)
+            if nargout > 1
+                Fi = inv(F);
+                invF_d_eta = -(Fi' * (Q \ Fi)); % deriv wrt eta of inv(F)
+                f_d_eta = Q \ b;                % deriv wrt eta of f
+                
+                d_eta = obj.epsilon - 0.5 * b' * f_d_eta + 0.5 * f' * invF_d_eta * f ...
+                    + f' * ( F \ f_d_eta ) - 0.5 * logDetQ + 0.5 * logDetF ...
+                    - 0.5 * (eta + omega) * trace(F \ Qi) + 0.5 * dimQ;
+                
+                gD = [d_eta
+                    -beta + 0.5 * dimQ + 0.5 * logDetF];
+            end
+        end
+        
+    end
+    
+end
---
+++ Utilities/Data/quadraticfit.m
@@ -0,0 +1,79 @@
+function [model, nmse] = quadraticfit(X, Y, varargin)
+% QUADRATICFIT Learns a quadratic model to fit input-output pairs (X, Y):
+% Y = X'*R*X + X'*r + r0 (R is symmetric).
+% For the likelihood of the linear model, a multiplicative noise model is 
+% used (the higher the absolute value of Y, the higher the variance).
+%
+%    INPUT
+%     - X           : [d x N] matrix, where N is the number of samples
+%     - Y           : [1 x N] vector
+%     - weights     : (optional) [1 x N] vector of samples weights
+%     - standardize : (optional) flag to standardize X and Y
+%
+%    OUTPUT
+%     - model       : struct with fields R, r, r0, dim (number of 
+%                     parameters of the model) and eval (function for 
+%                     generating samples, i.e., Y = model.eval(X))
+%     - nmse        : normalized mean squared error on the training data
+
+[d, N] = size(X);
+
+options = {'weights', 'standardize'};
+defaults = {ones(1,N), 0};
+[W, standardize] = internal.stats.parseArgs(options, defaults, varargin{:});
+
+D = d*(d+1)/2 + d + 1; % Number of parameters of the quadratic model
+
+if standardize
+    [Xn, Xmu, Xstd] = standardize_data(X, W);
+    [Yn, Ymu, Ystd] = standardize_data(Y, W);
+else
+    Xn = X;
+    Yn = Y;
+    Xmu = zeros(d,1);
+    Xstd = ones(d,1);
+    Ymu = zeros(1,1);
+    Ystd = ones(1,1);
+end
+
+% Generate feature vectors for linear regression
+Phi = basis_quadratic(d,Xn);
+
+% Weight each sample by its inverse of absoltute relative output
+absY = abs( Yn - max(Yn) - 1e-2 * (max(Yn) - min(Yn)) ); % Small regularizer for 0 output
+weight = 1 ./ absY;
+if all(absY == absY(1)), weight(:) = 1; end % If all output are the same
+W = W .* weight;
+
+% Get parameters by linear regression on pairs (Phi, Y)
+params = linear_regression(Phi, Yn, W);
+% params = bayesian_linear_regression(Phi, Yn, W);
+
+assert(~any(isnan(params)), 'Model fitting failed.')
+
+% Extract model params
+AQuadratic = zeros(d);
+ind = logical(tril(ones(d)));
+AQuadratic(ind) = params(d+2:end);
+R = (AQuadratic + AQuadratic') / 2;
+r = params(2:d+1);
+r0 = params(1);
+
+% De-standardize
+XstdMat = diag((1./Xstd));
+YstdMat = diag(Ystd);
+A = (XstdMat * R * XstdMat);
+newR = YstdMat * A;
+newr = -YstdMat * (2*A*Xmu - XstdMat*r);
+newr0 = YstdMat * (Xmu'*A*Xmu - r'*XstdMat*Xmu + r0) + Ymu;
+R = newR;
+r = newr;
+r0 = newr0;
+
+model.dim = D;
+model.R = R;
+model.r = r;
+model.r0 = r0;
+model.eval = @(X) sum( (X'*R)' .* X, 1 ) + (X'*r)' + r0';
+
+nmse = mean( ( Y - model.eval(X) ).^2 ) / mean( Y.^2 );
