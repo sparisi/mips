bb4ac46031b54ffb898302ce1137c8c1690f0581
Simone <parisi@ias.tu-darmstadt.de>
2016-04-29 18:49:30 +0200

    minor changes

--- Utilities/argmax.m
+++ Utilities/argmax.m
@@ -1,3 +1,12 @@
 function r = argmax(x, dim)
 
+if nargin == 1
+    if isvector(x)
+        [~, r] = max(x);
+        return
+    else
+        error('Not enough input arguments.')
+    end
+end
+
 [~, r] = max(x, [], dim);
