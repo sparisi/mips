4494b29bd9469936633e6970d83d2f469a613b89
Simone <parisi@ias.tu-darmstadt.de>
2016-05-21 11:12:58 +0200

    more generic myplot

--- myplot.m
+++ myplot.m
@@ -1,18 +1,24 @@
-% Script for comparing the results of different methods averaged over 
-% several trials. Data is in files named 'METHOD_TRIAL.mat' (1_1.mat,
-% 1_2.mat, ..., 2_2.mat, etc...).
+% Script for comparing results of different methods averaged over many trials. 
+% Data is stored in files named 'METHOD_TRIAL.mat' (1_1.mat, 1_2.mat, ..., 2_2.mat, etc...).
 
 close all
 clear all
 
-for METHOD = [1 3 4 10 11]
+%% Change this values according to your needs
+dataFOLDER = 'results/';
+methodsALL = [1 3 4];
+methodsNAMES = {'Method1', 'OtherMethod', 'SuperAlg'};
+entryNAME = 'J_history';
+
+%% Plot
+for method = methodsALL;
     
     counter = 1;
     dataMatrix = [];
-    for TRIAL = 1 : 999
+    for trial = 1 : 999
         try
-            load([num2str(METHOD) '_' num2str(TRIAL)])
-            dataMatrix(counter,:) = J_history; % Change to your desired data
+            load([dataFOLDER num2str(method) '_' num2str(trial)])
+            dataMatrix(counter,:) = eval(entryNAME);
             counter = counter + 1;
         catch
         end
@@ -24,7 +30,7 @@ for METHOD = [1 3 4 10 11]
         1:size(dataMatrix,2), ...
         mean(dataMatrix,1), ...
         std(dataMatrix), ...
-        {'LineWidth', 2, 'DisplayName', num2str(METHOD)}, ...
+        {'LineWidth', 2, 'DisplayName', methodsNAMES{methodsALL==method}}, ...
         0.1 );
     legend('-DynamicLegend');
     
