3c257e87ecbf432ac3b645e8b767627249e82c9c
sparisi <simone.parisi@mail.polimi.it>
2016-12-14 17:56:38 +0100

    new make_gifs

--- make_gifs.m
+++ make_gifs.m
@@ -1,25 +1,48 @@
+% Single plot
+
 close all
 clear all
 
-figure
-filename = 'animation.gif';
-dt = 0.1;
+filename = 'animation2';
+dt = 1; % Pause between each frame
+F = struct('cdata',[],'colormap',[]);
+
+fh = figure('units','pixels','position',[100 100 1092 384]);
+set(gcf,'color','w');
 %hold all % If you want to overlap plots
 
-data = rand(100,10); % Use your data here
-n = size(data,2);
-F(n) = struct('cdata',[],'colormap',[]);
+n = 10;
+data1 = rand(100,n); % Use your data here
 
+cont = 1;
 for i = 1 : n
-    plot(data(:,i))
-    F(i) = getframe;
-    [X, map] = rgb2ind(frame2im(F(i)),256);
+    plot(data1(:,i),'bo')
+    axis([0,100,0,1])
     
+    ax = gca;
+    ax.Units = 'pixels';
+    pos = ax.Position;
+    marg = 30;
+    rect = [-marg, -marg, pos(3)+2*marg, pos(4)+2*marg];
+    F(cont) = getframe(ax, rect);
+
+    % Pass axis instead of figure to getframe in order to capture only the
+    % inside of the plot. The margin can be used to get also axes ticks.
+    
+    [X, map] = rgb2ind(frame2im(F(cont)),256);
+    cont = cont + 1;
+    
+    % Write gif
     if i == 1
-        imwrite(X, map, filename, 'Loopcount', inf, 'DelayTime', dt)
+        imwrite(X, map, [filename '.gif'], 'Loopcount', inf, 'DelayTime', dt)
     else
-        imwrite(X, map, filename, 'WriteMode', 'Append', 'DelayTime', dt)
+        imwrite(X, map, [filename '.gif'], 'WriteMode', 'Append', 'DelayTime', dt)
     end
 end
 
-%movie(F)
+% Write video
+v = VideoWriter(filename, 'MPEG-4');
+v.FrameRate = 1/dt;
+open(v)
+writeVideo(v,F)
+close(v)
---
+++ make_gifs2.m
@@ -0,0 +1,45 @@
+% Multiple subplots
+
+close all
+clear all
+
+filename = 'animation2';
+dt = 1; % Pause between each frame
+F = struct('cdata',[],'colormap',[]);
+
+fh = figure('units','pixels','position',[100 100 1092 384]);
+set(gcf,'color','w');
+%hold all % If you want to overlap plots
+
+n = 10;
+data1 = rand(100,n); % Use your data here
+data2 = exp(data1);
+
+cont = 1;
+for i = 1 : n
+    subplot(1,2,1)
+    plot(data1(:,i),'bo')
+    axis([0,100,0,1])
+    
+    subplot(1,2,2)
+    plot(data2(:,i),'r--o')
+    axis([0,100,1,2])
+
+    F(cont) = getframe(fh);
+    [X, map] = rgb2ind(frame2im(F(cont)),256);
+    cont = cont + 1;
+    
+    % Write gif
+    if i == 1
+        imwrite(X, map, [filename '.gif'], 'Loopcount', inf, 'DelayTime', dt)
+    else
+        imwrite(X, map, [filename '.gif'], 'WriteMode', 'Append', 'DelayTime', dt)
+    end
+end
+
+% Write video
+v = VideoWriter(filename, 'MPEG-4');
+v.FrameRate = 1/dt;
+open(v)
+writeVideo(v,F)
+close(v)
