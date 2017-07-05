1f2dd92404220e79b633ed9a8d2df5565a8a55c5
sparisi <simone.parisi@mail.polimi.it>
2017-07-05 18:37:12 +0200

    exponential linear unit

---
+++ Algs/DeepRL/NeuralNetwork/Layers/ELU.m
@@ -0,0 +1,30 @@
+classdef ELU < Layer
+    
+    properties
+        alpha
+    end
+    
+    methods
+        
+        function obj = ELU(alpha)
+            obj.W = [];
+            obj.alpha = alpha;
+        end
+        
+        function Y = forward(obj, X)
+            Y = X;
+            idx = X < 0;
+            Y(idx) = obj.alpha * exp(X(idx) - 1);
+        end
+        
+        function [dX, dW] = backward(obj, dY)
+            dX = ones(size(obj.X));
+            idx = obj.X < 0;
+            dX(idx) = obj.alpha * exp(obj.X(idx));
+            dX = dX .* dY;
+            dW = [];
+        end
+        
+    end
+    
+end
