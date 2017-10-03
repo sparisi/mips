7a1c06d663903db1fcf5ca79de315a2fd69a6a26
sparisi <simone.parisi@mail.polimi.it>
2017-10-03 14:50:31 +0900

    loss function renamed

--- Algs/DeepRL/LossFun/crossentropy.m
+++ Algs/DeepRL/LossFun/crossentropy.m
@@ -1,5 +1,5 @@
-function [l, dl] = ce(y, t)
-% CE Cross entropy between predictions Y and targets T (both D x N 
+function [l, dl] = crossentropy(y, t)
+% CROSSENTROPY Cross entropy between predictions Y and targets T (both D x N 
 % matrices, where D is the dimensionality and N is the number of samples).
 
 l = -mean(t.*log(y) + (1-t).*log(1-y),2);
