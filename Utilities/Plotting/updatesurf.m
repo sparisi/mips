ac1f6dee4b5ae14a130beddf1b8270c6d5a06115
sparisi <simone.parisi@mail.polimi.it>
2017-10-04 16:32:38 +0900

    updatesurf

---
+++ Utilities/Plotting/updatesurf.m
@@ -0,0 +1,27 @@
+function updatesurf(name, X, Y, newZ)
+% UPDATESURF Updates surf plots without generating a new figure.
+%
+%    INPUT
+%     - name : figure name
+%     - X    : X as in surf
+%     - Y    : Y as in surf
+%     - newZ : Z as in surf
+
+% Look for a figure with the specified name
+fig = findobj('type','figure','name',name);
+
+% If the figure does not exist, create it and plot the surf
+if isempty(fig)
+    fig = figure();
+    fig.Name = name;
+    surf(X,Y,newZ)
+    title(name)
+    return
+end
+
+% Update Z values
+dataObjs = findobj(fig,'Type','Surface');
+set(dataObjs, 'CData', newZ);
+set(dataObjs, 'ZData', newZ);
+
+drawnow limitrate
