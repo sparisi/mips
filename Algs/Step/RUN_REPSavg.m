c43bf6b1fa94a27ae28efad4c4348051d51411de
sparisi <simone.parisi@mail.polimi.it>
2017-11-22 15:15:46 +0900

    reps avg with different length episodes

--- Algs/Step/RUN_REPSavg.m
+++ Algs/Step/RUN_REPSavg.m
@@ -9,13 +9,12 @@
 % Non-parametric Policy Search with Limited Information Loss (2017)
 
 bfsV = @(varargin)basis_poly(2,mdp.dstate,0,varargin{:});
-bfsV = @(varargin)basis_krbf(10, [mdp.stateLB, mdp.stateUB], 0, varargin{:});
+bfsV = @(varargin)basis_krbf(4, [mdp.stateLB, mdp.stateUB], 0, varargin{:});
 % bfsV = @(varargin)basis_krbf(10, 20*[-ones(dim,1), ones(dim,1)], 0, varargin{:});
 % bfsV = @(varargin)basis_rbf(5*[-ones(dim,1), ones(dim,1)], 0.5./[5; 5], 0, varargin{:});
 
 solver = REPSavg_Solver(0.5,bfsV);
 
-nmax = episodes_learn * steps_learn;
 data = [];
 varnames = {'r','s','nexts','a'};
 bfsnames = { {'phiP', @(s)policy.basis1(s)}, {'phiV', bfsV} };
@@ -26,15 +25,22 @@ while iter < 1500
     
     [ds, J] = collect_samples(mdp, episodes_learn, steps_learn, policy);
     entropy = policy.entropy([ds.s]);
+    nmax = size([ds.s],2);
     data = getdata(data,ds,nmax,varnames,bfsnames);
 
     % Ensure stationary distribution of transient dynamics by resetting 
-    % the system with a probability
-    reset_prob = 1/steps_learn; 
+    % the system with a probability 1/#steps.
+    steps_ep = [ds.length];
+    idx_init = [1 cumsum(steps_ep(1:end-1))+1];
+    reset_prob = zeros(1,nmax);
+    for i = 1 : episodes_learn
+        reset_prob(idx_init(i):idx_init(i)+steps_ep(i)) = 1./steps_ep(i);
+    end
+    reset_prob(end) = [];
 
     [d, divKL] = solver.optimize(data.r, data.phiV, ...
-        bsxfun(@plus, (1-reset_prob)*data.phiV_nexts, ... 
-        reset_prob*mean(data.phiV(:,1:steps_learn:end),2)));
+        bsxfun(@plus, (1-reset_prob).*data.phiV_nexts, ... 
+        reset_prob.*mean(data.phiV(:,idx_init),2)));
 
     policy = policy.weightedMLUpdate(d, data.a, data.phiP);
 
@@ -43,8 +49,10 @@ while iter < 1500
     fprintf('%d ) Entropy: %.2f, KL: %.2f, J: %.4f\n', iter, entropy, divKL, J(robj))
     
     iter = iter + 1;
+    
+    solver.plotV(mdp.stateLB, mdp.stateUB)
 end
 
 %%
 plot(J_history)
-show_simulation(mdp, policy.makeDeterministic, 100, 0.01)
+show_simulation(mdp, policy.makeDeterministic, 1000, 0.01)
