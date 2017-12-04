a54024eb842345fbb5652ef6e71025a42c22a254
sparisi <simone.parisi@mail.polimi.it>
2017-12-04 10:15:44 +0900

    added lqr to ddpg experiments

--- Algs/DeepRL/Experiments/DDPG_DoubleLink.m
+++ Algs/DeepRL/Experiments/DDPG_DoubleLink.m
@@ -11,9 +11,9 @@ steps_learn = 100;
 range = [mdp.stateLB, mdp.stateUB];
 m = mean(range,2);
 range_centered = bsxfun(@minus,range,m);
-preprocessS = @(s)bsxfun(@times, bsxfun(@minus,s,m), 1./range_centered(:,2))';
+preprocessS = @(s) bsxfun(@times, bsxfun(@minus,s,m), 1./range_centered(:,2))';
+% preprocessS = @(s) s';
 
-% Normalization in [-1,0]
-preprocessR = @(r)r/1e10;
+preprocessR = @(r) r;
 
 noise_std = 20;
---
+++ Algs/DeepRL/Experiments/DDPG_LQR.m
@@ -0,0 +1,22 @@
+clear
+close all
+
+%% Get problem specification
+mdp = LQR_v2(2);
+episodes_eval = 150;
+steps_eval = 150;
+steps_learn = 150;
+
+% Clip in [-300, -300] and normalization in [-1,1]
+mdp.stateLB(:) = -30;
+mdp.stateUB(:) = 30;
+range = [mdp.stateLB, mdp.stateUB];
+m = mean(range,2);
+range_centered = bsxfun(@minus,range,m);
+preprocessS = @(s)bsxfun(@times, bsxfun(@minus, ...
+    bsxfun(@max, bsxfun(@min,s,mdp.stateUB), mdp.stateLB), m), 1./range_centered(:,2))';
+
+% Clip at -300
+preprocessR = @(r) (bsxfun(@max, r, -300));
+
+noise_std = 1.2;
