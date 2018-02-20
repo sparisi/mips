315743434ac410ce80710cde6bbbef8db4bd1d37
parisi <parisi@ias.informatik.tu-darmstadt.de>
2018-02-20 20:38:10 +0100

    linear gmm

---
+++ Library/Policies/GMM/GmmLinear.m
@@ -0,0 +1,97 @@
+classdef GmmLinear < Policy
+% GMMLINEAR Gaussian Mixture Model with linear means and constant covariances.
+% Parameters: means, covariances and mixture weights.
+% Means are stored in rows, as in Matlab's GMDISTRIBUTION.
+%
+% As the number of components is variable (up to GMAX), this class does not
+% implement the properties THETA and DPARAMS.
+    
+    properties(GetAccess = 'public', SetAccess = 'private')
+        basis % basis functions
+        A     % means coefficients
+        Sigma % covariances
+        p     % mixing proportions
+        gmax  % max number of Gaussians
+    end
+    
+    methods
+        
+        %% Constructor
+        function obj = GmmLinear(varargin)
+            if nargin == 4 % initialize with a single Gaussian
+                obj.basis = varargin{1};
+                A0 = varargin{2};
+                Sigma0 = varargin{3};
+                obj.gmax = varargin{4};
+                for i = 1 : obj.gmax
+                    obj.A(:,:,i) = A0;
+                    obj.Sigma(:,:,i) = Sigma0;
+                end
+                obj.p = ones(1,obj.gmax) / obj.gmax;
+            elseif nargin == 5 % directly init with all components
+                obj.basis = varargin{1};
+                obj.A = varargin{2};
+                obj.Sigma = varargin{3};
+                obj.gmax = varargin{4};
+                obj.p = varargin{5};
+            else
+                error('Wrong number of input arguments.')
+            end
+            [d,m,k] = size(obj.A);
+            [d1,d2,d3] = size(obj.Sigma);
+            assert(d1 == d & d2 == d & d3 == k);
+            assert(m == obj.basis()+1);
+            obj.daction = size(obj.A,1);
+        end
+        
+        %% GMM.RANDOM
+        function action = drawAction(obj, States)
+            n = size(States,2);
+            components = mymnrnd(obj.p,n); % Select the Gaussians for drawing the samples
+            action = zeros(obj.daction,n);
+            phi = obj.basis_bias(States);
+            for i = 1 : length(obj.p)
+                idx = components == i;
+                action(:,idx) = mymvnrnd(obj.A(:,:,i) * phi(:,idx),obj.Sigma(:,:,i));
+            end
+        end
+        
+        %% GMM.PDF
+        function probability = evaluate(obj, States, Actions)
+            probability = zeros(1,size(Actions,2));
+            phi = obj.basis_bias(States);
+            for i = 1 : length(obj.p)
+                probability = probability + obj.p(i) * ...
+                    exp(loggausspdf(Actions, obj.A(:,:,i) * phi, obj.Sigma(:,:,i)));
+            end
+        end
+        
+        %% Entropy
+        function S = entropy(obj, States, Actions)
+            prob_list = obj.evaluate(States, Actions);
+            idx = isinf(prob_list) | isnan(prob_list) | prob_list == 0;
+            prob_list(idx) = 1; % ignore them -> log(1)*1 = 0
+            S = -mean(log(prob_list));
+        end
+        
+        %% GMM.FIT
+        function obj = weightedMLUpdate(obj, weights, Actions, Phi)
+            assert(min(weights>=0));
+            [~, gmm] = emgm_linear(Actions, Phi, obj.gmax, weights);
+            obj.p = gmm.ComponentProportion;
+            obj.A = gmm.A;
+            obj.Sigma = gmm.Sigma;
+        end
+
+        %% Change stochasticity
+        function obj = makeDeterministic(obj)
+            obj.Sigma = obj.Sigma * 1e-8;
+        end
+        
+        function obj = randomize(obj, factor)
+            obj.Sigma = obj.Sigma .* factor;
+        end
+       
+    end
+    
+end
---
+++ Utilities/Distributions/emgm_linear.m
@@ -0,0 +1,143 @@
+function [label, model, llh] = emgm_linear(X, Phi, init, W)
+% EMGM_LINEAR EM Weighted EM for fitting a Gaussian mixture model with 
+% linear means.
+%
+%    INPUT
+%     - X     : [D x N] data matrix
+%     - Phi   : [M x N] features matrix
+%     - init  : scalar K or 
+%               [1 x N] matrix of labels, where 1 <= label(i) <= K, or 
+%               model (struct)
+%     - W     : (optional) [1 x N] weights vector (1 by default)
+%
+%    OUTPUT
+%     - label : [1 x N] vector indicating which Gaussian is associated to
+%               each sample
+%     - model : struct with means (mu), covariances (Sigma) and component
+%               proportions of the mixture model
+%     - llh   : log-likelihood of the model
+%
+% =========================================================================
+% ACKNOWLEDGEMENT
+% http://www.mathworks.com/matlabcentral/fileexchange/26184-em-algorithm-for-gaussian-mixture-model
+
+if nargin < 4
+    W = ones(1,size(X,2));
+else
+    assert(min(W>=0), 'Weights cannot be negative.');
+end
+
+R = initialization(X, Phi, init);
+[~, label(1,:)] = max(R, [], 2);
+R = R(:, unique(label));
+
+tol = 1e-10;
+maxiter = 500;
+llh = -inf(1, maxiter);
+converged = false;
+t = 1;
+
+while ~converged && t < maxiter
+
+    t = t + 1;
+    model = maximization(X, Phi, R, W);
+    [R, llh(t)] = expectation(X, Phi, model);
+    
+    [~, label(:)] = max(R, [], 2);
+    u = unique(label);   % non-empty components
+    if size(R, 2) ~= size(u, 2)
+        R = R(:, u);     % remove empty components
+    else
+        converged = llh(t) - llh(t-1) < tol * abs(llh(t));
+    end
+    
+end
+
+llh = llh(2:t);
+if ~converged
+    warning('Not converged in %d steps.\n',maxiter);
+end
+
+end
+
+
+%% Init
+function R = initialization(X, Phi, init)
+
+[d,n] = size(X);
+if isstruct(init)         % initialize with a model
+    R = expectation(X, Phi, init);
+elseif length(init) == 1  % random initialization with k random components
+    A = zeros(d,size(Phi,1),init);
+    for k = 1 : init
+        idx = randsample(n,d);
+        A(:,:,k) = ( pinv(Phi(:,idx))' * X(:,idx) )';
+    end
+    model.A = A;
+    model.Sigma = repmat(nearestSPD(cov(X')),1,1,k);
+    model.ComponentProportion = ones(1,init)/init;
+    R = expectation(X, Phi, model);
+elseif size(init,1) == 1 && size(init,2) == n  % initialize with labels
+    label = init;
+    k = max(label);
+    R = full(sparse(1:n,label,1,n,k,n));
+else
+    error('Initialization is not valid.');
+end
+
+end
+
+%% Expectation
+function [R, llh] = expectation(X, Phi, model)
+
+A = model.A;
+Sigma = model.Sigma;
+w = model.ComponentProportion;
+
+n = size(X,2);
+k = length(model.ComponentProportion);
+logRho = zeros(n,k);
+
+for i = 1 : k
+    logRho(:,i) = loggausspdf_lin(X, Phi, A(:,:,i), Sigma(:,:,i));
+end
+
+logRho = bsxfun(@plus, logRho, log(w));
+T = logsumexp(logRho,2);
+llh = sum(T) / n; % loglikelihood
+logR = bsxfun(@minus, logRho, T);
+R = exp(logR);
+
+end
+
+
+%% Maximization
+function model = maximization(X, Phi, R, W)
+
+d = size(X,1);
+f = size(Phi,1);
+k = size(R,2);
+R = bsxfun(@times,R,W');
+nk = sum(R,1)';
+w = nk / sum(W);
+
+Sigma = zeros(d,d,k);
+A = zeros(d,f,k);
+sqrtR = sqrt(R);
+for i = 1 : k
+    Z = (sum(R(:,i))^2 - sum(R(:,i).^2)) / sum(R(:,i))^2;
+    PhiD = bsxfun(@times,Phi,R(:,i)');
+    A(:,:,i) = ((PhiD * Phi' + 1e-8 * eye(f)) \ PhiD * X')';
+    mu = A(:,:,i) * Phi;
+    Xo = bsxfun(@minus, X, mu);
+    Xo = bsxfun(@times, Xo, sqrtR(:,i)');
+    Sigma(:,:,i) = Xo * Xo' / nk(i);
+    if Z ~= 0, Sigma(:,:,i) = Sigma(:,:,i) / Z; end
+    Sigma(:,:,i) = nearestSPD(Sigma(:,:,i));
+end
+
+model.A = A;
+model.Sigma = Sigma;
+model.ComponentProportion = w';
+
+end
