5d18091cf79972611c2c3f7d17677040ce37efbd
parisi <parisi@ias.informatik.tu-darmstadt.de>
2018-11-08 11:41:37 +0100

    retrace

--- Library/retrace.m
+++ Library/retrace.m
@@ -1,15 +1,16 @@
-function A = gae(data, V, gamma, lambda)
-% Computes generalized advantage estimates from on-policy data.
+function A = retrace(data, V, gamma, lambda, prob_ratio)
+% Computes generalized advantage estimates from off-policy data.
 % Data have to be ordered by episode: data.r must have first all samples 
 % from the first episode, then all samples from the second, and so on.
 % So you cannot use samples collected with COLLECT_SAMPLES2.
 %
 % =========================================================================
 % REFERENCE
-% J Schulman, P Moritz, S Levine, M Jordan, P Abbeel
-% High-Dimensional Continuous Control Using Generalized Advantage Estimation
-% ICLR (2017)
+% R Munos, T Stepleton, Anna Harutyunyan, M G Bellemare
+% Safe and efficient off-policy reinforcement learning
+% NIPS (2016)
 
+prob_ratio = min(1, prob_ratio);
 r = [data.r];
 t = [data.t];
 t(end+1) = 1;
@@ -17,8 +18,8 @@ A = zeros(size(V));
 
 for k = size(V,2) : -1 : 1
     if t(k+1) == 1 % Next state is a new episode init state
-        A(k) = r(k) - V(k);
+        A(k) = prob_ratio(k) * (r(k) - V(k));
     else
-        A(k) = r(k) + gamma*V(k+1) - V(k) + gamma*lambda*A(k+1);
+        A(k) = prob_ratio(k) * (r(k) + gamma*V(k+1) - V(k) + gamma*lambda*A(k+1));
     end
 end
