813081b3e95c63622efc87a3f9e5ccfd773b6370
parisi <parisi@ias.informatik.tu-darmstadt.de>
2019-03-22 21:13:39 +0100

    plot_all for tensorl

---
+++ plot_all_dat.m
@@ -0,0 +1,18 @@
+% This script iterative loads data from .dat files located in FOLDER into 
+% a matrix, and plots the column VAR_IDX. 
+
+close all
+
+folder = '/home/sparisi/tensorl/data-trial/ppo/Pendulum-v0/';
+Files = dir(fullfile(folder,'*.dat'));
+
+varbound = -inf;
+var_idx = 6;
+
+figure, hold all
+for current_file = {Files.name}
+    h = load([folder current_file{:}]);
+    plot(max(h(:,var_idx), varbound))
+end
+
+legend({Files.name}, 'Interpreter', 'none')
