82821e0e02c1492fad1bf614f62777f160ae29bb
parisi <parisi@ias.informatik.tu-darmstadt.de>
2019-03-24 17:43:16 +0100

    var renaming

--- Algs/REPS/COPOS.m
+++ Algs/REPS/COPOS.m
@@ -29,20 +29,20 @@ while iter < 250
     s = [ds.s];
     a = [ds.a];
     
-    % Estimate MC returns and fit R = phi*w to estimate the natural gradient w
+    % Estimate MC returns and fit R = psi*w to estimate the natural gradient w
     R = mc_ret(ds,mdp.gamma);
     R = R(robj,:);
     R = (R - mean(R)) / std(R); % standardize data
-    phi = policy.dlogPidtheta(s,a);
-    w = fminunc(@(w)mse_linear(w,phi,R), policy.theta, options_mse);
+    psi = policy.dlogPidtheta(s,a);
+    w = fminunc(@(w)mse_linear(w,psi,R), policy.theta, options_mse);
 
-    % Compute Q for all possible actions for all sampled states
-    Q = zeros(length(policy.action_list), size(s,2));
+    % Compute A for all possible actions for all sampled states
+    A = zeros(length(policy.action_list), size(s,2));
     for i = policy.action_list
-        Q(i,:) = w' * policy.dlogPidtheta(s, repmat(i, 1, size(s,2)));
+        A(i,:) = w' * policy.dlogPidtheta(s, repmat(i, 1, size(s,2)));
     end
 
-    % Compute entropy for COPOS bound
+    % Compute current entropy for COPOS entropy bound
     probs_old = policy.distribution(s);
     probs_old(probs_old==0) = 1e-8;
     H_old = -mean(sum(probs_old.*log(probs_old),1));
@@ -50,11 +50,11 @@ while iter < 250
     % Solve COPOS dual
     dual = @(x) x(1) * epsilon ...
         - x(2) * (H_old - beta) ...
-        + (x(1) + x(2)) * mean(logsumexp((x(1) * log(probs_old) + Q) / (x(1) + x(2)), 1));
-    [params,~,~,info] = fmincon(dual, [eta;omega], ...
+        + (x(1) + x(2)) * mean(logsumexp((x(1) * log(probs_old) + A) / (x(1) + x(2)), 1));
+    [x,~,~,info] = fmincon(dual, [eta;omega], ...
         [], [], [], [], [1e-8;1e-8], [1e8;1e8], [], options_dual);
-    eta = params(1);
-    omega = params(2);
+    eta = x(1);
+    omega = x(2);
 
     % Update and evaluate policy
     J = evaluate_policies(mdp, episodes_eval, steps_eval, policy.makeDeterministic);
